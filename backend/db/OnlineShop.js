7e1e16f5f5be5d4f9f96b65d12fcca6f09f39bf9
zidehsaraee <moein.zideh@gmail.com>
2024-09-16 20:37:32 +0100

    fix mysql connection

--- backend/db/OnlineShop.js
+++ backend/db/OnlineShop.js
@@ -1,15 +1,37 @@
 require("dotenv").config();
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
-const OnlineShopDB = mysql.createConnection(
-  urlDB
-  //   {
-  //   host: process.env.DB_HOST,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASS,
-  //   database: process.env.DB_NAME,
-  // }
-);
+// const OnlineShopDB = mysql.createConnection(
+//     {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//   }
+// );
+
+const mysql = require("mysql2");
+
+const OnlineShopDB = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+OnlineShopDB.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL!");
+});
+
+// Example query
+OnlineShopDB.query("SELECT * FROM users", (err, results) => {
+  if (err) throw err;
+  console.log("Users:", results);
+});
 
 module.exports = OnlineShopDB;
